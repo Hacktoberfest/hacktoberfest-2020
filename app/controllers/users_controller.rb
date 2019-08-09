14e40dcd6ee21a93a5bc83eff4b19e1ceb1e2163
Frida Casas <fridakahsas@yahoo.com>
2019-08-09 16:17:24 -0400

    Setting up users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+def index
+  users = User.order("id ASC")
+  render json: users
+end
+
+def show
+  user = User.find_by_id(params[:id])
+
+  if user.nil?
+    render json: { error: "User for id #{params[:id]} not found" },
+           status: :not_found
+  else
+    render json: user
+  end
+end
+
+def create
+  user = (User.new, params)
+
+  if user.save
+    render json: user
+  else
+    render json: { errors: user.errors.full_messages }, status: :bad_request
+  end
+end
+
+end
