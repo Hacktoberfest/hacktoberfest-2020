df58bf2ce0537255c5c40416801518b71e4bab7f
Frida Casas <fridakahsas@yahoo.com>
2019-08-12 19:29:56 -0400

    adding methods for current user as well as logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
+  end
+
+  def logged_in?
+    !@current_user.nil?
+  end
 end
