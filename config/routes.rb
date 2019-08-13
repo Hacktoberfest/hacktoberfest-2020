877795b03286352a09f9fc79254e9c986bb227ef
Frida Casas <fridakahsas@yahoo.com>
2019-08-13 14:25:56 -0400

    home view and controller for index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   get "/auth/:provider/callback", to: "sessions#create"
+  get "/logout" => "sessions#destroy", as: :logout
     get "/login" => redirect("/auth/github"), as: :login
-    get "/logout" => "sessions#destroy", as: :logout
 end
