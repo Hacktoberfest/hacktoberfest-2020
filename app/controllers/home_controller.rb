607346e5c50a3d739d2991ebfc5430b98e3353de
Frida Casas <fridakahsas@yahoo.com>
2019-08-13 14:21:39 -0400

    resetting controllers out of api folder

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    current_user
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def create
     @user = User.where(gh_id: auth_hash[:uid]).first_or_create
     session[:current_user_id] = @user.id
