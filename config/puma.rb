cbbb2a5c82da663e35ef6c353934ade57ac62d76
Frida Casas <fridakahsas@yahoo.com>
2019-08-16 14:02:47 -0400

    fix spacing

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
-port        ENV.fetch('PORT') { 3000 }
+port ENV.fetch('PORT') { 3000 }
 
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
