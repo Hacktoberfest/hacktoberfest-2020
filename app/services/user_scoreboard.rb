301cf5282d17d5fe6aa3d055f6c2f91fe4736628
Frida Casas <40510669+fridaland@users.noreply.github.com>
2019-08-22 08:37:41 -0400

    remove pry

--- app/services/user_scoreboard.rb
+++ app/services/user_scoreboard.rb
@@ -12,7 +12,6 @@ class UserScoreboard
   end
 
   def score
-    binding.pry
     client = Octokit::Client.new(access_token: user.provider_token)
     prs = client.search_issues("is:pr user: #{client.user.name}")
     prs.total_count
