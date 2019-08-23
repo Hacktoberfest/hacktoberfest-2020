3067c71514498527cb1cf2a5651c8598f71cd280
Chris Ewald <chrisewald@gmail.com>
2019-08-23 14:31:41 -0400

    Using graphql library to get users' PRs.
    Created a PORO object so we can work with top level Pull Request objects from
    the graphql API. Needs new PullRequestFilterService object.

--- app/services/user_scoreboard.rb
+++ app/services/user_scoreboard.rb
@@ -5,6 +5,27 @@ require 'octokit'
 class UserScoreboard
   attr_reader :user, :start_date, :end_date
 
+  SCOREBOARD_QUERY = <<~GRAPHQL
+    query($username:String!) {
+      user(login: $username) {
+        pullRequests(states: OPEN last: 100) {
+          nodes {
+            id
+            title
+            createdAt
+            labels(first: 100) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
+
   def initialize(user, start_date, end_date)
     @start_date = start_date
     @end_date = end_date
@@ -12,8 +33,11 @@ class UserScoreboard
   end
 
   def score
-    client = Octokit::Client.new(access_token: user.provider_token)
-    prs = client.search_issues("is:pr user: #{client.user.name}")
-    prs.total_count
+    client = GithubGraphqlApiClient.new(access_token: user.provider_token)
+    response = client.request(SCOREBOARD_QUERY, username: user.name)
+    prs = response.data.user.pullRequests.nodes.map{ |pr| GraphqlPullRequest.new(pr) }
+
+    # New Filter service object here
+    42
   end
 end
