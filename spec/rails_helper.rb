434a80850322a3a41c0944c58d407922fd3dd93d
Frida Casas <fridakahsas@yahoo.com>
2019-08-24 20:45:00 -0400

    add pull_request_filter_helper to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -80,7 +80,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.include AuthenticationHelper
-
+  config.include PullRequestFilterHelper
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
