7a4cbf47af2ddb48b11adf3f66031266e2244c7c
Frida Casas <fridakahsas@yahoo.com>
2019-08-29 00:20:36 -0400

    update invalid date array in PR helper

--- spec/support/pull_request_filter_helper.rb
+++ spec/support/pull_request_filter_helper.rb
@@ -23,7 +23,7 @@ ARRAY_WITH_INVALID_DATES = [
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.',
     'url' => 'https://github.com/syl20bnr/spacemacs/pull/6012',
     'createdAt' => '2019-10-08T06:24:38Z',
-    'labels' => ['Invalid'] },
+    'labels' => [] },
   { 'id' => 'MDExOlB1bGxSZXF1ZXN0OTA4ODAzMzQ=',
     'title' => 'Coercion type systems',
     'body' =>
@@ -31,7 +31,7 @@ ARRAY_WITH_INVALID_DATES = [
     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.',
     'url' => 'https://github.com/intridea/hashie/pull/379',
     'createdAt' => '2019-10-25T19:59:18Z',
-    'labels' => ['Invalid'] }
+    'labels' => [] }
 ].freeze
 
 ARRAY_WITH_INVALID_DATES_AND_INVALID_LABEL = [
