b8c406ad98ddfe36b6b1979e6c7078839247757f
Frida Casas <fridakahsas@yahoo.com>
2019-09-03 13:21:19 -0400

    refactoring pull request service specs, linting

--- app/services/pull_request_service.rb
+++ app/services/pull_request_service.rb
@@ -9,33 +9,32 @@ class PullRequestService
   end
 
   def all
-    # Create this service when we lookup spammy repos in order to lookup
-    # all Repo Spammy states in SQL query
+    # Create this service when we lookup spammy repos:
+    # in order to lookup all Repo Spammy states in SQL query
     # prs = PullRequestStateLookupService.new(filtered_github_pull_requests)
-    filtered_github_pull_requests.map { |ghpr| PullRequest.new(ghpr) }
+    filtered_github_pull_requests(github_pull_requests).map do |ghpr|
+      PullRequest.new(ghpr)
+    end
   end
 
   def all_by_state
-    hash = {
+    {
       invalid: all.select { |p| p.label_names.include?('invalid') },
       eligible: all.reject { |p| p.label_names.include?('invalid') }
-     }
-    hash
+    }
   end
 
   def score
     all_by_state[:eligible].count || 0
   end
 
-  private
-
   def github_pull_requests
     github_prs = GithubPullRequestService.new(user)
     github_prs.pull_requests
   end
 
-  def filtered_github_pull_requests
-    github_pull_requests.select do |e|
+  def filtered_github_pull_requests(prs)
+    prs.select do |e|
       e.created_at >= ENV['START_DATE'] && e.created_at <= ENV['END_DATE']
     end
   end
