7c54c5c48b61adeca3792b862af6bfee7d7fcca1
Frida Casas <fridakahsas@yahoo.com>
2019-09-03 18:05:55 -0400

    updated pr_service specs, linting

--- spec/services/pull_request_service_spec.rb
+++ spec/services/pull_request_service_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 RSpec.describe PullRequestService do
   let(:user) { FactoryBot.create(:user) }
-  let(:scoreboard) { PullRequestService.new(user) }
+  let(:pr_service) { PullRequestService.new(user) }
 
   describe '.new' do
     context 'valid arguments' do
       it 'returns a UserScoreBoard' do
-        expect(scoreboard).to_not be(nil)
+        expect(pr_service).to_not be(nil)
       end
     end
 
@@ -27,76 +27,107 @@ RSpec.describe PullRequestService do
     end
   end
 
-  describe '#all' do
-    before do
-      mock_authentication(uid: user.uid)
-    end
-
-    context 'it ruturns an array of PullRequest objects' do
-    end
-  end
-
   describe '#all_by_state' do
-    context 'it returns a pull request hash with state keys' do
-      it 'will filter each pull request and assign it to the right key' do
+    context 'given an array with valid pull_requests' do
+      before do
+        allow(pr_service)
+          .to receive(:github_pull_requests)
+          .and_return(pull_request_data(PR_DATA[:valid_array]))
+      end
+
+      it 'will categorize prs as eligible', vcr: { record: :new_episodes } do
+        pr_service.all
+        expect(pr_service.all_by_state[:eligible].length).to eq(4)
+      end
+    end
+
+    context 'given an array with invalid pull_requests' do
+      before do
+        allow(pr_service)
+          .to receive(:github_pull_requests)
+          .and_return(pull_request_data(PR_DATA[:array_with_invalid_labels]))
+      end
+
+      it 'will categorize invalid prs', vcr: { record: :new_episodes } do
+        pr_service.all
+        expect(pr_service.all_by_state[:invalid].length).to eq(2)
       end
     end
   end
 
-  describe '#github_pull_requests' do
-    context 'the GithubPullRequestService is called' do
-      it 'makes a new GithubPullRequestService' do
-      end
-
-      it 'returns an array of GithubPullRequests objects' do
-      end
-    end
-  end
-
-  describe '#filtered_github_pull_requests' do
+  describe '#all' do
     context 'given an array of 4 pull requests' do
       context 'pull requests with valid dates and valid labels' do
+        before do
+          allow(pr_service)
+            .to receive(:github_pull_requests)
+            .and_return(pull_request_data(PR_DATA[:valid_array]))
+        end
         it 'filters and returns all 4 PRs', vcr: { record: :new_episodes } do
-          prs = pull_request_data(PR_DATA[:valid_array])
-          expect(scoreboard.filtered_github_pull_requests(prs).length).to eq(4)
+          expect(pr_service.all.length).to eq(4)
         end
       end
 
       context 'pull requests with 2 invalid dates & valid labels' do
+        before do
+          allow(pr_service)
+            .to receive(:github_pull_requests)
+            .and_return(pull_request_data(PR_DATA[:array_with_invalid_dates]))
+        end
         it 'filters and returns 2 of the PRs', vcr: { record: :new_episodes } do
-          prs = pull_request_data(PR_DATA[:array_with_invalid_dates])
-          expect(scoreboard.filtered_github_pull_requests(prs).length).to eq(2)
+          expect(pr_service.all.length).to eq(2)
         end
       end
 
       context 'pull_requests with valid dates & 2 invalid labels' do
-        it 'filters and returns 2 of the PRs', vcr: { record: :new_episodes } do
-          prs = pull_request_data(PR_DATA[:array_with_invalid_labels])
-          expect(scoreboard.filtered_github_pull_requests(prs).length).to eq(2)
+        before do
+          allow(pr_service)
+            .to receive(:github_pull_requests)
+            .and_return(pull_request_data(PR_DATA[:array_with_invalid_labels]))
+        end
+        it 'filters and returns 4 of the PRs', vcr: { record: :new_episodes } do
+          expect(pr_service.all.length).to eq(4)
         end
       end
 
       context 'pull_requests with 4 invalid dates & invalid labels' do
+        before do
+          allow(pr_service)
+            .to receive(:github_pull_requests)
+            .and_return(pull_request_data(PR_DATA[:invalid_array]))
+        end
         it 'filters & returns an empty array', vcr: { record: :new_episodes } do
-          prs = pull_request_data(PR_DATA[:invalid_array])
-          expect(scoreboard.filtered_github_pull_requests(prs).length).to eq(0)
+          expect(pr_service.all.length).to eq(0)
         end
       end
     end
   end
 
   describe '#score' do
-    # subject { PullRequestService.new(user) }
-    # context 'a new user with no pull requests' do
-    #   it 'returns 0', vcr: { record: :new_episodes } do
-    #     expect(subject.score).to eq(0)
-    #   end
-    # end
-    #
-    # context 'it counts the amount of pull requests' do
-    #   it 'returns an integer', vcr: { record: :new_episodes } do
-    #     expect(subject.score).to be_a(Integer)
-    #   end
-    # end
+    context 'a new user with no eligible pull requests' do
+      before do
+        allow(pr_service)
+          .to receive(:github_pull_requests)
+          .and_return(pull_request_data(PR_DATA[:invalid_array]))
+      end
+      it 'returns 0', vcr: { record: :new_episodes } do
+        pr_service.all
+        pr_service.all_by_state
+        expect(pr_service.score).to eq(0)
+      end
+    end
+
+    context 'it counts the amount of pull requests' do
+      before do
+        allow(pr_service)
+          .to receive(:github_pull_requests)
+          .and_return(pull_request_data(PR_DATA[:valid_array]))
+      end
+      it 'returns the total', vcr: { record: :new_episodes } do
+        pr_service.all
+        pr_service.all_by_state
+        expect(pr_service.score).to eq(4)
+      end
+    end
   end
 end
