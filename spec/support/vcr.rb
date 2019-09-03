ab50d26101eeca7c9054c8874a117d2a4c1c28f1
Chris Ewald <chrisewald@gmail.com>
2019-09-03 15:35:09 -0400

    Filter out sensitive access token data from VCR output

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,4 +6,8 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  c.filter_sensitive_data("<TEST_USER_GITHUB_TOKEN>") do
+    user_github_token
+  end
 end
