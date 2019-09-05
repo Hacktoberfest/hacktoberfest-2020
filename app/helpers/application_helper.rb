539d1981994f9e6b961ec0131ecbe07c3ca2ddfa
Frida Casas <fridakahsas@yahoo.com>
2019-09-05 17:21:02 -0400

    add html_safe to markdown method for content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/OutputSafety
 module ApplicationHelper
   def markdown(source)
-    Kramdown::Document.new(source.to_s).to_html
+    Kramdown::Document.new(source.to_s).to_html.html_safe
   end
 end
+# rubocop:enable Rails/OutputSafety
