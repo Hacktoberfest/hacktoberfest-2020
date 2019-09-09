ac7057bafabdd9bb327e329762906d0d58ebbff2
Frida Casas <fridakahsas@yahoo.com>
2019-09-09 18:47:15 -0400

    add segment initializer

---
+++ config/initializers/segment.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'segment/analytics'
+
+Analytics = Segment::Analytics.new({
+  write_key: ENV['SEGMENT_WRITE_KEY'],
+  on_error: Proc.new { |status, msg| print msg }
+})
