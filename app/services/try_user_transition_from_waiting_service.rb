dc58d1291cc69c0083fd98edc2619912e19ddb7c
Jacob Haff <jacobhaff2@gmail.com>
2019-09-12 15:29:32 -0400

    Add guard clause to TryUserTransitionFromWaitingService

--- app/services/try_user_transition_from_waiting_service.rb
+++ app/services/try_user_transition_from_waiting_service.rb
@@ -2,6 +2,8 @@
 
 module TryUserTransitionFromWaitingService
   def self.call(user)
+    return unless user.state == 'waiting'
+
     return if user.complete
 
     user.ineligible if user.eligible_pull_requests_count < 4
