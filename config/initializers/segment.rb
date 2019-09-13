030541b86a2f1694e4490faf3d460400536e81dd
Frida Casas <fridakahsas@yahoo.com>
2019-09-13 16:25:22 -0400

    Remove stub: true from Segment

--- config/initializers/segment.rb
+++ config/initializers/segment.rb
@@ -4,7 +4,6 @@ require 'segment/analytics'
 
 # stub_value = if Rails.env.test?; stub: true; end
 Analytics = Segment::Analytics.new(
-  stub: true,
   write_key: ENV['SEGMENT_WRITE_KEY'],
   on_error: proc { |_status, msg| msg }
 )
