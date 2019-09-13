6e95b34f20056fcd38e5da8944055a509603e84d
Chris Ewald <chrisewald@gmail.com>
2019-09-13 13:27:36 -0400

    Use Module rather than String as argument to RSpec.describe

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'UsersController', type: :request do
+RSpec.describe UsersController, type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:pr_service) { PullRequestService.new(current_user) }
   let(:controller) { UsersController.new }
