b5370e0bec6124b81a01808e17a4f4e55a181680
Chris Ewald <chrisewald@gmail.com>
2019-09-16 13:03:50 -0400

    rubocop -a

--- spec/jobs/user_transition_job_spec.rb
+++ spec/jobs/user_transition_job_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UserTransitionJob, type: :job do
   ActiveJob::Base.queue_adapter = :test
-  
+
   let(:user) { FactoryBot.create(:user) }
 
   it 'tries to transition the user' do
