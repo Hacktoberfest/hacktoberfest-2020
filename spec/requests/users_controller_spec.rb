e3092d12573552521c8e22f6bb75da2a40348444
Jacob Haff <jacobhaff2@gmail.com>
2019-09-16 17:01:23 -0400

    Change vcr call to :vcr in user controller spec

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe UsersController, type: :request do
         login
       end
 
-      it 'transitions the user to the completed state', vcr: { record: :new_episodes } do
+      it 'transitions the user to the completed state', :vcr do
         get profile_path
         user.reload
         expect(user.state).to eq('completed')
@@ -56,19 +56,19 @@ RSpec.describe UsersController, type: :request do
 
       include_examples 'tries transition'
 
-      it 'returns 4 as the max score', vcr: { record: :new_episodes } do
+      it 'returns 4 as the max score', :vcr do
         get profile_path
         expect(response.body).to include('4 out of 4')
         expect(response).to be_successful
       end
 
-      it 'only shows 4 valid pull requests', vcr: { record: :new_episodes } do
+      it 'only shows 4 valid pull requests', :vcr do
         get profile_path
         fifth_eligible_pr = PR_DATA[:valid_array].last
         expect(response.body).to_not include(fifth_eligible_pr['title'])
       end
 
-      it 'transitions the user to the waiting state', vcr: { record: :new_episodes } do
+      it 'transitions the user to the waiting state', :vcr do
         allow_any_instance_of(User)
           .to receive(:eligible_pull_requests_count).and_return(4)
 
@@ -86,12 +86,12 @@ RSpec.describe UsersController, type: :request do
 
       include_examples 'tries transition'
 
-      it 'only displays progress', vcr: { record: :new_episodes } do
+      it 'only displays progress', :vcr do
         get profile_path
         expect(response.body).to include('0 out of 4')
       end
 
-      it 'keeps the user in the registered state', vcr: { record: :new_episodes } do
+      it 'keeps the user in the registered state', :vcr do
         allow_any_instance_of(User)
           .to receive(:eligible_pull_requests_count).and_return(0)
 
@@ -112,12 +112,12 @@ RSpec.describe UsersController, type: :request do
 
       include_examples 'tries transition'
 
-      it 'calculates score of 3 valid PRs', vcr: { record: :new_episodes } do
+      it 'calculates score of 3 valid PRs', :vcr do
         get profile_path
         expect(response.body).to include('3 out of 4')
       end
 
-      it 'returns all pull requests', vcr: { record: :new_episodes } do
+      it 'returns all pull requests', :vcr do
         get profile_path
         invalid_pr = PR_DATA[:invalid_array].first
         expect(response.body).to include(invalid_pr['title'])
