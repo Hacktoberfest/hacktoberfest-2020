1effa7e1fb46f91a800ee1f6d8a19809203633f3
Chris Ewald <chrisewald@gmail.com>
2019-09-16 17:30:58 -0400

    Add CreateLanguages migration

---
+++ db/migrate/20190916212332_create_languages.rb
@@ -0,0 +1,10 @@
+class CreateLanguages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :languages do |t|
+      t.string :name, null: false
+      t.index :name, unique: true
+
+      t.timestamps null: false
+    end
+  end
+end
