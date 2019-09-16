b98ea7080bc1ec1d039fe2377f31342afdf3f16d
Chris Ewald <chrisewald@gmail.com>
2019-09-16 13:01:37 -0400

    Fix migration syntax for rubocop
    No DB changes needed and migrations work up and down

--- db/migrate/20190820183954_add_confirmation_and_marketing_emails_to_user.rb
+++ db/migrate/20190820183954_add_confirmation_and_marketing_emails_to_user.rb
@@ -2,7 +2,8 @@
 
 class AddConfirmationAndMarketingEmailsToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :terms_acceptance, :boolean, default: false
-    add_column :users, :marketing_emails, :boolean, default: false
+    change_table :users do |t|
+      t.boolean :terms_acceptance, :marketing_emails, default: false
+    end
   end
 end
