cf92f3268aea161ad44c147ad3b90949f37dd121
Jacob Haff <jacobhaff2@gmail.com>
2019-09-16 16:58:29 -0400

    Add tests for checking user transition on User#show

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.shared_examples 'tries transition'  do
+RSpec.shared_examples 'tries transition' do
   it 'tries to transition the user' do
     expect(TryUserTransitionService).to receive(:call).and_return(true)
     get profile_path
@@ -18,6 +18,30 @@ RSpec.describe UsersController, type: :request do
     before do
       mock_authentication(uid: user.uid)
       login
+    end
+
+    context 'a waiting user has 4 mature pull_requests' do
+      before do
+        prs = pull_request_data(PR_DATA[:mature_array]).map do |pr|
+          PullRequest.new(pr)
+        end
+
+        allow_any_instance_of(User).to receive(:pull_requests).and_return(prs)
+        allow_any_instance_of(User)
+          .to receive(:eligible_pull_requests_count).and_return(4)
+        allow_any_instance_of(User)
+          .to receive(:mature_pull_requests_count).and_return(4)
+
+        user.wait
+        mock_authentication(uid: user.uid)
+        login
+      end
+
+      it 'transitions the user to the completed state', vcr: { record: :new_episodes } do
+        get profile_path
+        user.reload
+        expect(user.state).to eq('completed')
+      end
     end
 
     context 'a user has more than 4 eligible pull requests' do
@@ -43,6 +67,15 @@ RSpec.describe UsersController, type: :request do
         fifth_eligible_pr = PR_DATA[:valid_array].last
         expect(response.body).to_not include(fifth_eligible_pr['title'])
       end
+
+      it 'transitions the user to the waiting state', vcr: { record: :new_episodes } do
+        allow_any_instance_of(User)
+          .to receive(:eligible_pull_requests_count).and_return(4)
+
+        get profile_path
+        user.reload
+        expect(user.state).to eq('waiting')
+      end
     end
 
     context 'a user has no pull_requests' do
@@ -56,6 +89,15 @@ RSpec.describe UsersController, type: :request do
       it 'only displays progress', vcr: { record: :new_episodes } do
         get profile_path
         expect(response.body).to include('0 out of 4')
+      end
+
+      it 'keeps the user in the registered state', vcr: { record: :new_episodes } do
+        allow_any_instance_of(User)
+          .to receive(:eligible_pull_requests_count).and_return(0)
+
+        get profile_path
+        user.reload
+        expect(user.state).to eq('registered')
       end
     end
 
