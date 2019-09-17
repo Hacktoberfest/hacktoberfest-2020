f7add05f8142ecd5e1d32986c79672b556b75fc0
Chelsea Kent <chelsea.kent@gmail.com>
2019-09-17 10:05:57 -0700

    Set up of initial header styles and font-family

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ Bundler.require(*Rails.groups)
 module Hacktoberfest
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
