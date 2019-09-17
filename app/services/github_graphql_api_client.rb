31ab1b7a2ec5e87a26d4ab5485e1fbcb9b982f8a
Chris Ewald <chrisewald@gmail.com>
2019-09-17 15:47:09 -0400

    Allow our GithubGraphqlApi to take params in multiple styles
    This allows us to use existing app code directly along with our newer style. I
    prefer our app style, but editing our code here allows us to directly use code
    copied from the existing app

--- app/services/github_graphql_api_client.rb
+++ app/services/github_graphql_api_client.rb
@@ -14,10 +14,20 @@ class GithubGraphqlApiClient
   end
 
   def request(query, variables = {})
+    query, variables = normalize_hash_args(query, variables)
     response = client.post(GITHUB_GRAPHQL_API_URL,
                            { query: query, variables: variables }.to_json,
                            'Authorization': "bearer #{@access_token}",
                            'Content-Type': 'application/json')
     Hashie::Mash.new(JSON.parse(response.body))
+  end
+
+  private
+
+  def normalize_hash_args(query, variables)
+    return [query, variables] unless query.is_a?(Hash)
+
+    variables = query.delete(:variables)
+    [query[:query], variables]
   end
 end
--- app/services/hacktoberfest_project_query_composer.rb
+++ app/services/hacktoberfest_project_query_composer.rb
@@ -5,8 +5,8 @@ class HacktoberfestProjectQueryComposer
       pagination_clause = ", after:\"#{cursor}\""
     end
     {
-      "query" => "query FindHacktoberfestIssues($queryString: String!) { rateLimit { cost limit remaining resetAt } search(query: $queryString, type: ISSUE, first: #{results_per_page}#{pagination_clause}) { issueCount pageInfo { endCursor hasNextPage } edges { node { ... on Issue { bodyText databaseId number title url participants { totalCount } timeline { totalCount } repository { databaseId description name nameWithOwner url primaryLanguage { name } stargazers { totalCount } watchers { totalCount } forks { totalCount } codeOfConduct { url } } } } } } }",
-      "variables" => { "queryString" => "state:open label:hacktoberfest" }
-    }.to_json
+      query: "query FindHacktoberfestIssues($queryString: String!) { rateLimit { cost limit remaining resetAt } search(query: $queryString, type: ISSUE, first: #{results_per_page}#{pagination_clause}) { issueCount pageInfo { endCursor hasNextPage } edges { node { ... on Issue { bodyText databaseId number title url participants { totalCount } timeline { totalCount } repository { databaseId description name nameWithOwner url primaryLanguage { name } stargazers { totalCount } watchers { totalCount } forks { totalCount } codeOfConduct { url } } } } } } }",
+      variables: { "queryString" => "state:open label:hacktoberfest" }
+    }
   end
 end
