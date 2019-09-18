ce5b7b9808accb97541bda53b245203afb09cc60
Jacob Haff <jacobhaff2@gmail.com>
2019-09-18 17:32:44 -0400

    Wire up sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+end if Rails.env.production?
+mount Sidekiq::Web, at: "/sidekiq"
+
 Rails.application.routes.draw do
   root to: 'pages#index'
   get '/auth/:provider/callback', to: 'sessions#create'
