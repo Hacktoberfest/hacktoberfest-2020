443ec4cddd25a8c9df89d3c1728eb5e4140991c4
Frida Casas <fridakahsas@yahoo.com>
2019-09-19 15:05:00 -0400

    Remove protected line

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,8 +62,6 @@ class User < ApplicationRecord
     score > 4 ? 4 : score
   end
 
-  protected
-
   def eligible_pull_requests_count
     pr_service = PullRequestService.new(self)
     pr_service.eligible_prs.count
@@ -86,28 +84,3 @@ class User < ApplicationRecord
     Hacktoberfest.end_date.past?
   end
 end
-
-
-
-
-
-#Coupon service
-#in complete or inclomplete (coupons might still be available)
-  #take a user and assign a user a coupon
-   #coupon type logic here 
-
-#validations user can only have one kind of sticker
-
-## Coupons
-# Sticker, T-Shirt
-# coupon - unclaimed - no user id etc.
-
-#User has_one sticker_coupon
-
-#User has_one shirt_coupon
-
-#User validates only one coupon
-  #coupon method on User returns either sticker coupon || shirt || nil
-  #based on this appropriate hacktoberfest ends partial in displayed
-
-# ENSURE NO RACE CONDITIONS
