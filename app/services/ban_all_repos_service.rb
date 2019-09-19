458dde9add2de7ca1f23d285277ba75a59c46b44
Jacob Haff <jacobhaff2@gmail.com>
2019-09-19 18:37:53 -0400

    Move all logic for finding repo ids to ban into service object, simplify job

---
+++ app/services/ban_all_repos_service.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module BanAllReposService
+  def self.call
+    repo_ids_to_ban.map do |repo_id|
+      if repo = Repository.find_by_gh_database_id(repo_id)
+        repo.update(Banned?: true)
+      end
+    end
+  end
+
+  def repo_ids_to_ban
+    repos = AirrecordTable.new.table('Spam Repos')
+    # binding.pry
+    repos.all.map do |repo|
+      repo.fields['Repo ID']
+    end
+  end
+end
