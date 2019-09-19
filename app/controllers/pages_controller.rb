f676cdf09d517c412cf490bb28a3ac096f2e6662
lpercivalDEV <lpercival.dev@gmail.com>
2019-09-19 16:40:06 -0400

    Begin set up for rendering issues on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 
 class PagesController < ApplicationController
   def index
-    @meetups = meetups.sort_by { |e| e['Event Start Date/Time'] }.first(4) 
+    @meetups = meetups.sort_by { |e| e['Event Start Date/Time'] }.first(4)
     # @meetups = meetups.select do |m|
     #   m.fields.key?('Featured?')
     # end.first(4)
