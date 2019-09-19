e050748579962dccdeb94e3ed01b66d2542b0014
Chelsea Kent <chelsea.kent@gmail.com>
2019-09-19 13:08:24 -0700

    Styles for tab

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,12 +1,21 @@
-$('#tabs li a').click(function() {
-  var t = $(this).attr('id');
-  console.log(t);
-  if ($(this).hasClass('inactive')) {
-    //this is the start of our condition
-    $('#tabs li a').addClass('inactive');
-    $(this).removeClass('inactive');
+$(document).ready(function() {
+  $('#tabs li a:first').addClass('selected');
+  $('#tabs li a:not(:first)').addClass('inactive');
+  $('.tab-block').hide();
+  $('.tab-block:first').show();
 
-    $('.panel-block').hide();
-    $('#' + t + 'C').fadeIn('slow');
-  }
+  $('#tabs li a').click(function() {
+    var t = $(this).attr('id');
+    $('#tabs li a').removeClass('selected');
+
+    if ($(this).hasClass('inactive')) {
+      //this is the start of our condition
+      $('#tabs li a').addClass('inactive');
+      $(this).removeClass('inactive');
+      $(this).addClass('selected');
+
+      $('.tab-block').hide();
+      $('#' + t + 'C').fadeIn('slow');
+    }
+  });
 });
