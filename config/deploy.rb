444848bfd0ae6068b95d1458491ebf9da7f984e6
Chris Ewald <chrisewald@gmail.com>
2019-09-21 17:20:47 -0400

    Basic info for project

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "hacktoberfest"
+set :repo_url, "git@github.com:raise-dev/hacktoberfest.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
