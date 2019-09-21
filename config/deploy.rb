54c2bff9bf85ccb6e92d12f437b203958b501b63
Chris Ewald <chrisewald@gmail.com>
2019-09-21 19:04:33 -0400

    Deploy directly to our deploy user home directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:raise-dev/hacktoberfest.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/hacktoberfest"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
