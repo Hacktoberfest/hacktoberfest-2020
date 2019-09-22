80f77d5966e580a3bf9c43081697e5fcf0ad53e8
Chris Ewald <chrisewald@gmail.com>
2019-09-22 00:47:35 -0400

    Revert "Add secret key base to production and staging config"
    This reverts commit e7f45cc5e67e0646d1a0cde92ec7ac910b640a01.
    
    Using the Rails 5.2 credentials system now instead. Must now
    set the RAILS_MASTER_KEY env variable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,8 +11,6 @@ Rails.application.configure do
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
-
   config.assets.js_compressor = :uglifier
 
   config.assets.compile = false
