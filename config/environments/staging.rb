1a9768928a09e33273829cf2fd3511059ed2e65c
Chris Ewald <chrisewald@gmail.com>
2019-09-22 00:21:04 -0400

    Copy production env config to staging

---
+++ config/environments/staging.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  config.cache_classes = true
+
+  config.eager_load = true
+
+  config.consider_all_requests_local = false
+
+  config.action_controller.perform_caching = true
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+
+  config.active_storage.service = :local
+
+  config.log_level = :debug
+
+  config.log_tags = [:request_id]
+
+  config.action_mailer.perform_caching = false
+
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+
+  config.log_formatter = ::Logger::Formatter.new
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.active_record.dump_schema_after_migration = false
+
+  config.assets.precompile += %w( '.svg' )
+
+  # Must include to get inline SVGs to work in deploy
+  config.assets.css_compressor = :sass
+end
