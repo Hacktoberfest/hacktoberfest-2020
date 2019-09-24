57ef1716fcc9bb33223208a7ab0ac3946a3e28f3
Chris Ewald <chrisewald@gmail.com>
2019-09-23 22:20:27 -0400

    IssueUpdateService
    Spec file does not currently have the happy path tested, but it works and its
    component services are covered

---
+++ app/services/issue_update_service.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module IssueUpdateService
+  module_function
+
+  def call(issue)
+    access_token = GithubTokenService.random
+    api_client = GithubGraphqlApiClient.new(access_token: access_token)
+    issue_state_fetcher = IssueStateFetcher.new(api_client: api_client)
+    begin
+      state = issue_state_fetcher.fetch!(issue)
+      IssueStateUpdater.update(issue: issue, state: state)
+    rescue IssueStateFetcherInvalidRepoError => fetch_error
+      issue.destroy
+    end
+  end
+end
