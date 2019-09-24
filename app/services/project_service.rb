e0376127bb21787873dacdf24c4e1e705fa12d34
Chris Ewald <chrisewald@gmail.com>
2019-09-23 20:36:26 -0400

    Fix N+1 query

--- app/services/project_service.rb
+++ app/services/project_service.rb
@@ -6,6 +6,7 @@ module ProjectService
   def sample(sample_size = 1)
     issues = Issue
              .open_issues_with_unique_permitted_repositories
+             .includes(repository: :language)
              .order(quality: :desc)
              .limit(sample_size * 10)
              .sample(sample_size)
