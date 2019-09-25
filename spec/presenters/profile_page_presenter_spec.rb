329b3e16bfbebdbfa554d6efa9035d48d65823b4
Frida Casas <fridakahsas@yahoo.com>
2019-09-25 16:38:31 -0400

    Expectations for specs

--- spec/presenters/profile_page_presenter_spec.rb
+++ spec/presenters/profile_page_presenter_spec.rb
@@ -4,14 +4,26 @@ require 'rails_helper'
 
 describe ProfilePagePresenter do
   context 'Hacktoberfest is in pre launch' do
+    it 'renders the pre_launch partial' do
 
+    end
   end
 
   context 'Hacktoberfest is active' do
+    it 'renders the profile timeline partial' do
 
+    end
   end
 
-  context 'Hacktoberfest has ended' do
+  context 'Hacktoberfest has ended and the user has won' do
+    it 'renders the results_partial' do
 
+    end
+  end
+
+  context 'Hacktoberfest has ended and the user has not won' do
+    it 'renders the profile timeline partial' do
+
+    end
   end
 end
