e25f952536cc38a07ae8d4ab9081ae4cd06c5139
lpercivalDEV <lpercival.dev@gmail.com>
2019-09-25 10:50:29 -0400

    Fixed visibility toggle

---
+++ app/assets/javascripts/visibility-toggle-faq.js
@@ -0,0 +1,30 @@
+// Toggle visibility of FAQ items on click
+
+window.onload = function() {
+  // Select all elements with 'expand' class and hold in array
+  var question =  document.getElementsByClassName("question-answer");
+  var answer = document.getElementsByClassName("hidden");
+
+  // Expand all questions
+  // for(var i = 0; i< question.length; i++){
+  //   question[i].addEventListener("click", function(){
+  //     for(var j=0; j<answer.length;j++){
+  //       answer[j].classList.toggle("show");
+  //     }
+  //   })
+  // }
+
+  // Expand single target item
+  for(var i = 0; i< question.length; i++){
+    question[i].addEventListener("click", function() {
+
+      //FIXME: THIS CODE WORKS BUT ONLY IF YOU CLICK CERTAIN AREA OF BLOCK (SLIGHTLY BELOW THE CHEVRON )
+      let temp = this;
+      let content = temp.querySelector(".answer");
+      console.log(content);
+        content.classList.toggle("hidden");
+
+    })
+  }
+
+}
