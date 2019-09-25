3170badf32021c937e8d5022a78c8b2d84749182
Frida Casas <fridakahsas@yahoo.com>
2019-09-25 15:29:46 -0400

    Profile page presenter for partial renders

---
+++ app/presenters/profile_page_presenter.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ProfilePagePresenter
+
+  def initialize(user)
+    @user = user
+  end
+
+  def display_pre_launch?
+    Hacktoberfest.pre_launch?
+  end
+
+  def display_timeline?
+     Hacktoberfest.active?
+  end
+
+  def  dispay_results?
+     Hacktoberfest.ended?
+  end
+
+  def score
+    @user.score
+  end
+end
