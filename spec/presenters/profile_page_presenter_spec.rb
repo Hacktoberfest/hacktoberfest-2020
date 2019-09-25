d82023f80c972f6fc7ffc1142ad0efe03ec1ae0b
Frida Casas <fridakahsas@yahoo.com>
2019-09-25 15:28:34 -0400

    Contexts for Profile Page Presenter

---
+++ spec/presenters/profile_page_presenter_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProfilePagePresenter do
+  context 'Hacktoberfest is in pre launch' do
+
+  end
+
+  context 'Hacktoberfest is active' do
+
+  end
+
+  context 'Hacktoberfest has ended' do
+
+  end
+end
