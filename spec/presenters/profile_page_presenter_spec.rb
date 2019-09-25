1f2f80d21e1248d03244a720dc968c5164c04272
Frida Casas <fridakahsas@yahoo.com>
2019-09-25 18:36:27 -0400

    Stubbing constants to test profile displays

--- spec/presenters/profile_page_presenter_spec.rb
+++ spec/presenters/profile_page_presenter_spec.rb
@@ -7,6 +7,11 @@ describe ProfilePagePresenter do
   let(:profile_presenter) { ProfilePagePresenter.new(user) }
 
   context 'Hacktoberfest is in pre launch' do
+    before do
+      allow(Hacktoberfest).to receive(:pre_launch?).and_return(true)
+      allow(Hacktoberfest).to receive(:active?).and_return(false)
+      allow(Hacktoberfest).to receive(:ended?).and_return(false)
+    end
     it 'displays the pre_launch partial' do
       expect(profile_presenter.display_pre_launch?).to eq(true)
     end
@@ -16,11 +21,16 @@ describe ProfilePagePresenter do
     end
 
     it 'does not display the results_partial' do
-      expect(profile_presenter.display_pre_launch?).to eq(false)
+      expect(profile_presenter.display_results?).to eq(false)
     end
   end
 
   context 'Hacktoberfest is active' do
+    before do
+      allow(Hacktoberfest).to receive(:active?).and_return(true)
+      allow(Hacktoberfest).to receive(:pre_launch?).and_return(false)
+      allow(Hacktoberfest).to receive(:ended?).and_return(false)
+    end
     it 'displays the profile timeline partial' do
       expect(profile_presenter.display_timeline?).to eq(true)
     end
@@ -35,6 +45,12 @@ describe ProfilePagePresenter do
   end
 
   context 'Hacktoberfest has ended and the user has won' do
+    before do
+      allow(Hacktoberfest).to receive(:ended?).and_return(true)
+      allow(Hacktoberfest).to receive(:pre_launch?).and_return(false)
+      allow(Hacktoberfest).to receive(:active?).and_return(false)
+      allow(user).to receive(:won_hacktoberfest?).and_return(true)
+    end
     it 'displays the results_partial' do
       expect(profile_presenter.display_results?).to eq(true)
     end
@@ -49,6 +65,13 @@ describe ProfilePagePresenter do
   end
 
   context 'Hacktoberfest has ended and the user has not won' do
+    before do
+      allow(Hacktoberfest).to receive(:ended?).and_return(true)
+      allow(Hacktoberfest).to receive(:pre_launch?).and_return(false)
+      allow(Hacktoberfest).to receive(:active?).and_return(false)
+      allow(user).to receive(:won_hacktoberfest?).and_return(false)
+    end
+
     it 'displays the profile timeline partial' do
       expect(profile_presenter.display_timeline?).to eq(true)
     end
