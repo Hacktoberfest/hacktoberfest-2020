27f315d375c3c35bd2ded716eb85b17ed42b517a
lpercivalDEV <lpercival.dev@gmail.com>
2019-09-25 13:26:00 -0400

    Add chevron style and expand all functionality (commented out)

--- app/assets/javascripts/visibility-toggle-faq.js
+++ app/assets/javascripts/visibility-toggle-faq.js
@@ -4,15 +4,17 @@ window.onload = function() {
   // Select all elements with 'expand' class and hold in array
   var question =  document.getElementsByClassName("question-answer");
   var answer = document.getElementsByClassName("hidden");
+  // var expandAll = document.getElementsByClassName("exColAll")[0];
+
 
   // Expand all questions
-  // for(var i = 0; i< question.length; i++){
-  //   question[i].addEventListener("click", function(){
-  //     for(var j=0; j<answer.length;j++){
-  //       answer[j].classList.toggle("show");
-  //     }
-  //   })
-  // }
+
+    // expandAll.addEventListener("click", function(){
+    //   for(var j=0; j<answer.length;j++){
+    //     answer[j].classList.toggle("show");
+    //   }
+    // })
+
 
   // Expand single target item
   for(var i = 0; i< question.length; i++){
