090805de0fa18ce7757780318a311d40b30313c7
Frida Casas <fridakahsas@yahoo.com>
2019-09-26 13:34:28 -0400

    Refactor ProfilePagePresenter

--- app/presenters/profile_page_presenter.rb
+++ app/presenters/profile_page_presenter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ProfilePagePresenter
+  attr_reader :user
 
   def initialize(user)
     @user = user
@@ -10,23 +11,19 @@ class ProfilePagePresenter
     Hacktoberfest.pre_launch?
   end
 
-  def display_timeline?
-    Hacktoberfest.active? || (Hacktoberfest.ended? && !@user.won_hacktoberfest?)
-  end
-
   def display_results?
     Hacktoberfest.ended? && @user.won_hacktoberfest?
   end
 
   def timeline_pull_requests
-    counter = 0
-    @user.pull_requests.take_while do |pr|
-      counter += 1 if pr.eligible?
-      counter <= 4
-    end
+    @user.timeline_pull_requests
   end
 
   def score
     @user.score
+  end
+
+  def name
+    @user.name
   end
 end
