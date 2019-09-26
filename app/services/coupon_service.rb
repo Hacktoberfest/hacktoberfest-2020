22f4da334d6a6fffb35f969ac6fa5f7365b01842
Frida Casas <fridakahsas@yahoo.com>
2019-09-26 17:30:20 -0400

    Coupon service refactor

--- app/services/coupon_service.rb
+++ app/services/coupon_service.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 class CouponService
-
   def initialize(user)
     @user = user
   end
 
   def assign_coupon
-    if ShirtCoupon.all.count > 0
-      coupon = ShirtCoupon.first
-      @user.shirt_coupon = coupon
-      coupon.update(user_id: @user.id)
-    elsif ShirtCoupon.all.count == 0
-      coupon = StickerCoupon.first
-      @user.sticker_coupon = coupon
-      coupon.update(user_id: @user.id)
-    elsif ShirtCoupon.all.count == 0 && StickerCoupon.all.count == 0
-      #leave empty or add new column 
-    end
+    assign_shirt_coupon
+    assign_sticker_coupon
   end
+
+  def assign_shirt_coupon
+    coupon = ShirtCoupon.first_available
+    coupon.user = @user
+  end
+
+  def assign_sticker_coupon
+    coupon = StickerCoupon.first_available
+    coupon.user = @user
+  end
+
 end
