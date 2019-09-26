fbe3e1e9f52f0daa183424a8b02a9fb3c29c25d1
Jacob Haff <jacobhaff2@gmail.com>
2019-09-26 18:00:25 -0400

    Add #status and #merged? methods to PullRequest

--- app/models/github_pull_request.rb
+++ app/models/github_pull_request.rb
@@ -21,6 +21,10 @@ class GithubPullRequest
     @graphql_hash.url
   end
 
+  def state
+    @graphql_hash.state
+  end
+
   def created_at
     @graphql_hash.createdAt
   end
--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -7,10 +7,10 @@ class PullRequest
     @github_pull_request = github_pull_request
   end
 
-  delegate :id, :title, :body, :url, :created_at,
+  delegate :id, :title, :body, :url, :state, :created_at,
            :label_names, to: :github_pull_request
 
-  def state
+  def status
     if label_names.include?('invalid')
       'invalid'
     else
@@ -18,13 +18,21 @@ class PullRequest
     end
   end
 
+  def state
+    @github_pull_request.state
+  end
+
   def eligible?
-    state == 'eligible'
+    status == 'eligible'
   end
 
   def mature?
     pr_date = DateTime.parse(@github_pull_request.created_at)
 
     pr_date < (DateTime.now - Hacktoberfest.pull_request_maturation_days)
+  end
+
+  def merged?
+    state == 'MERGED'
   end
 end
