c39758b32efa4b030b6ba18f87ebb20f6a0af358
Frida Casas <fridakahsas@yahoo.com>
2019-09-27 17:28:59 -0400

    Jquery function to filter displayed projects (wip)

---
+++ app/assets/javascripts/issue_filter.js
@@ -0,0 +1,15 @@
+$(function() {
+ $('#language_id').change(function(event){
+   var languageId = $(event.currentTarget).val();
+   var url = "/languages/projects/" + languageId;
+   $.ajax({
+     url: url
+   }).success(function(htmlData) {
+     $('#project-list').html(htmlData);
+   });
+ });
+});
+
+function updateFilterText() {
+  //toggle button visibility
+}
