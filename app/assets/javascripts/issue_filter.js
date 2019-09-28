9a33dd6bf87806871c2dfa1a000ce0e7d44e9a75
Frida Casas <fridakahsas@yahoo.com>
2019-09-28 14:07:17 -0400

    JQuery function succesfully populating issue container

--- app/assets/javascripts/issue_filter.js
+++ app/assets/javascripts/issue_filter.js
@@ -1,13 +1,15 @@
 $(function() {
- $('#language_id').change(function(event){
-   var languageId = $(event.currentTarget).val();
-   var url = "/languages/projects/" + languageId;
-   $.ajax({
-     url: url
-   }).done(function(htmlData) {
-     $('#project-list').html(htmlData);
-   });
- });
+  $('#language_id').change(function(event) {
+    var languageId = $(event.currentTarget).val();
+    var url = "/languages/projects/" + languageId;
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: function(htmlData) {
+        $('#project-list').html(htmlData);
+      }
+    })
+  })
 });
 
 function updateFilterText() {
