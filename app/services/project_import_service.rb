48a6ebe691d08a273c7b15ed55a8aed727623062
Chris Ewald <chrisewald@gmail.com>
2019-09-28 22:08:32 -0400

    Big refactor and accepting query string input

--- app/services/project_import_service.rb
+++ app/services/project_import_service.rb
@@ -3,11 +3,11 @@
 module ProjectImportService
   module_function
 
-  def call
+  def call(query_string = nil)
     access_token = GithubTokenService.random
     api_client = GithubGraphqlApiClient.new(access_token: access_token)
     fetcher = HacktoberfestProjectFetcher.new(api_client: api_client)
-    fetcher.fetch!
+    fetcher.fetch!(query_string)
     projects = fetcher.projects
     importer = HacktoberfestProjectImporter.new
     importer.import_all(projects)
