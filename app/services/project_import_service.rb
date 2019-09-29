616c9117b593fe17298dcabec78f867c6ef4326d
Chris Ewald <chrisewald@gmail.com>
2019-09-28 20:05:51 -0400

    Move rake task into a new ProjectImportService

---
+++ app/services/project_import_service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module ProjectImportService
+  module_function
+
+  def call
+    access_token = ENV.fetch('GITHUB_ACCESS_TOKEN')
+    api_client = GithubGraphqlApiClient.new(access_token: access_token)
+    fetcher = HacktoberfestProjectFetcher.new(api_client: api_client)
+    fetcher.fetch!
+    projects = fetcher.projects
+    importer = HacktoberfestProjectImporter.new
+    importer.import_all(projects)
+  rescue HacktoberfestProjectFetcherError => e
+    log_error(e.message)
+  rescue StandardError => e
+    log_error(e.message)
+  end
+
+  def log_error(str)
+    Rails.logger.error str
+  end
+end
