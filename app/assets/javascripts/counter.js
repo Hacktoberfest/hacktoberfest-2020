0fcc3cd14315e0cbf23cdaa5331d5272ed507ad6
Frida Casas <fridakahsas@yahoo.com>
2019-09-30 13:11:26 -0400

    Update js counter to show all contents only once

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -1,6 +1,6 @@
 var days, hours, minutes, seconds;
 
-function countdownTimers (elements, date) {
+function countdownTimers (element, date) {
   setInterval(function () {
     // find the amount of "seconds" between now and target
     var current_date = new Date().getTime()
@@ -13,10 +13,7 @@ function countdownTimers (elements, date) {
     seconds_left = seconds_left % 3600;
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);
-    for(i=0; i < elements.length; i++) {
-      var element = elements[i];
-      countdown = days + "d:" + hours + "h:" + minutes + "m:" + seconds + "s";
-      element.innerHTML = countdown
-    }
+    countdown = days + "d:" + hours + "h:" + minutes + "m:" + seconds + "s";
+    element.innerHTML = "🎉 You’ve submitted the four required PRs for the Hacktoberfest challenge! So long as your PRs successfully pass the review period, they’ll become valid in:" + countdown + " — which means you’ll have officially completed this year’s challenge!"
   }, 1000);
 }
