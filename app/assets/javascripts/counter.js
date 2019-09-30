ae7735ff456eb32cf17c01c5a0243f0f748f5a6b
Frida Casas <fridakahsas@yahoo.com>
2019-09-30 17:30:43 -0400

    Updating styles & winning message elements

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -1,6 +1,6 @@
 var days, hours, minutes, seconds;
 
-function countdownTimers (element, date) {
+function countdownTimers (elements, date) {
   setInterval(function () {
     // find the amount of "seconds" between now and target
     var current_date = new Date().getTime()
@@ -14,6 +14,8 @@ function countdownTimers (element, date) {
     minutes = parseInt(seconds_left / 60);
     seconds = parseInt(seconds_left % 60);
     countdown = days + "d:" + hours + "h:" + minutes + "m:" + seconds + "s";
-    element.innerHTML = "🎉 You’ve submitted the four required PRs for the Hacktoberfest challenge! So long as your PRs successfully pass the review period, they’ll become valid in:" + countdown + " — which means you’ll have officially completed this year’s challenge!"
+    elements[0].innerHTML = "🎉 You’ve submitted the four required PRs for the Hacktoberfest challenge! So long as your PRs successfully pass the review period, they’ll become valid in "
+    elements[1].innerHTML =  countdown
+    elements[2].innerHTML =  " — which means you’ll have officially completed this year’s challenge! 🎉"
   }, 1000);
 }
