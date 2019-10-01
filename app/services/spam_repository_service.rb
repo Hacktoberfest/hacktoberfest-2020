560912050277aade3b3e882a8ceb30dbedcc7917
Frida Casas <fridakahsas@yahoo.com>
2019-10-01 16:03:09 -0400

    Ensure spammy repos have been verified

--- app/services/spam_repository_service.rb
+++ app/services/spam_repository_service.rb
@@ -5,8 +5,8 @@ module SpamRepositoryService
 
   def call(repo_id)
     table = AirrecordTable.new.table('Spam Repos')
-    spam_repo_ids = table.all.map do |row|
-      row["Repo ID"].to_i
+    spam_repo_ids = table.all.map do |repo|
+      repo['Repo ID']&.to_i if repo['Verified?']
     end
     spam_repo_ids.include?(repo_id)
   end
