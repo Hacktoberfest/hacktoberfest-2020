105eefebdf994e5d45097a4c33edf662abaa9ee3
Frida Casas <fridakahsas@yahoo.com>
2019-10-01 16:36:44 -0400

    Update spam_id_array to not contain nil value

--- app/services/spam_repository_service.rb
+++ app/services/spam_repository_service.rb
@@ -7,7 +7,7 @@ module SpamRepositoryService
     table = AirrecordTable.new.table('Spam Repos')
     spam_repo_ids = table.all.map do |repo|
       repo['Repo ID']&.to_i if repo['Verified?']
-    end
+    end.compact
     spam_repo_ids.include?(repo_id)
   end
 end
