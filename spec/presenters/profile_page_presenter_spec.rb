a20a283ef8c914e3154e3e428d20d36af9fe1c96
Frida Casas <fridakahsas@yahoo.com>
2019-10-03 15:33:59 -0400

    Improve profile presenter specs

--- spec/presenters/profile_page_presenter_spec.rb
+++ spec/presenters/profile_page_presenter_spec.rb
@@ -4,7 +4,12 @@ require 'rails_helper'
 
 describe ProfilePagePresenter do
   let(:user) { FactoryBot.create(:user) }
+  let(:shirt_winner) { FactoryBot.create(:user, :won_shirt) }
+  let(:sticker_winner) { FactoryBot.create(:user, :won_sticker) }
   let(:profile_presenter) { ProfilePagePresenter.new(user) }
+  let(:won_shirt_presenter) { ProfilePagePresenter.new(shirt_winner) }
+  let(:won_sticker_presenter) { ProfilePagePresenter.new(sticker_winner) }
+  let(:incomplete_user_presenter) { ProfilePagePresenter.new(incomplete_user) }
 
   context 'Hacktoberfest is in pre launch' do
     before do
@@ -37,7 +42,7 @@ describe ProfilePagePresenter do
       expect(profile_presenter.display_winners?).to eq(true)
     end
 
-    it 'does not display the participants partial' do
+    it 'does not display participant partial', vcr: { record: :new_episodes } do
       expect(profile_presenter.display_participants?).to eq(false)
     end
 
@@ -47,53 +52,46 @@ describe ProfilePagePresenter do
   end
 
   context 'the user has won a shirt' do
-    before do
-      let(:shirt_winner) { FactoryBot.create(:user, :won_shirt) }
-    end
-
     it 'assigns the user a shirt coupon' do
-      binding.pry
-      expect(shirt_winner.shirt_coupon).to exist
+      expect(shirt_winner.shirt_coupon).to_not be(nil)
     end
 
     it 'returns a coupon code for the user' do
-      expect(profile_presenter.show_coupon).to_not be(nil)
+      expect(won_shirt_presenter.show_coupon).to_not be(nil)
     end
   end
 
   context 'the user has won a sticker' do
-    before do
-      let(:sticker_winner) { FactoryBot.create(:user, :won_sticker) }
-    end
-
     it 'assigns the user a sticker coupon' do
-      binding.pry
-      expect(sticker_winner.sticker_coupon).to exist
+      expect(sticker_winner.sticker_coupon).to_not be(nil)
     end
 
     it 'returns a coupon code for the user' do
-      expect(profile_presenter.show_coupon).to_not be(nil)
+      expect(won_sticker_presenter.show_coupon).to_not be(nil)
     end
   end
 
-  context 'Hacktoberfest has ended and the user has not won' do
+  context 'Hacktoberfest has ended the user has not won' do
+    let(:incomplete_user) { FactoryBot.create(:user, :incompleted) }
     before do
+      allow(Hacktoberfest).to receive(:end_date).and_return(Date.today - 7)
       allow(Hacktoberfest).to receive(:ended?).and_return(true)
       allow(Hacktoberfest).to receive(:pre_launch?).and_return(false)
       allow(Hacktoberfest).to receive(:active?).and_return(false)
-      allow(user).to receive(:won_hacktoberfest?).and_return(false)
+      allow(incomplete_user).to receive(:hacktoberfest_ended?).and_return(true)
+      allow(incomplete_user).to receive(:won_hacktoberfest?).and_return(false)
     end
 
-    it 'displays the participants partial' do
-      expect(profile_presenter.display_participants?).to eq(true)
+    it 'displays the participants partial', vcr: { record: :new_episodes } do
+      expect(incomplete_user_presenter.display_participants?).to eq(true)
     end
 
     it 'does not display the results_partial' do
-      expect(profile_presenter.display_winners?).to eq(false)
+      expect(incomplete_user_presenter.display_winners?).to eq(false)
     end
 
     it 'does not display the pre_launch partial' do
-      expect(profile_presenter.display_pre_launch?).to eq(false)
+      expect(incomplete_user_presenter.display_pre_launch?).to eq(false)
     end
   end
 end
