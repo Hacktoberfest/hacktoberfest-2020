843a15be343a7a22c4270f5f3f31684a4a9f97a4
John Britton <public@johndbritton.com>
2019-10-05 19:25:46 -0400

    create retryable github api client

---
+++ spec/services/github_retryable_graphql_api_client.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GithubRetryableGraphqlApiClient do
+  describe '#request' do
+    context 'the first request is successful' do
+      before do
+        allow_any_instance_of(GithubGraphqlApiClient).
+          to receive(:request).and_return(Hashie::Mash.new)
+      end
+
+      it 'returns the results from the API' do
+        client = GithubRetryableGraphqlApiClient.new(access_token: 'some-token')
+        response = client.request(mock_query)
+        expect(response).to be_an_instance_of(Hashie::Mash)
+      end
+    end
+
+    context 'the first two requests fail and the third succeeds' do
+      before do
+        response_values = [:raise, :raise, Hashie::Mash.new]
+        allow_any_instance_of(GithubGraphqlApiClient).
+          to receive(:request) do
+            v = response_values.shift
+            v == :raise ? raise(Faraday::ClientError, "the server responded with status 502") : v
+        end
+
+        allow(GithubTokenService).to receive(:random).and_return('random-one', 'random-two', 'random-three')
+      end
+
+      context 'retries is set to zero' do
+        let(:client) { GithubRetryableGraphqlApiClient.new(access_token: 'some-token') }
+
+        it 'raises an error' do
+          expect { client.request(mock_query) }.to raise_error(Faraday::ClientError)
+        end
+      end
+
+      context 'retries is set to three' do
+        let(:client) { GithubRetryableGraphqlApiClient.new(access_token: 'some-token', retries: 3) }
+
+        it 'returns the results from the API' do
+          response = client.request(mock_query)
+          expect(response).to be_an_instance_of(Hashie::Mash)
+        end
+      end
+    end
+  end
+end
