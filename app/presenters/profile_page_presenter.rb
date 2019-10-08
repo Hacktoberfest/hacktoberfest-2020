d67cb6e23ff5c02f6eb2eeb622ae4eb00bf642ff
Frida Casas <fridakahsas@yahoo.com>
2019-10-08 17:27:49 -0400

    Lint all presenters

--- app/presenters/profile_page_presenter.rb
+++ app/presenters/profile_page_presenter.rb
@@ -56,10 +56,9 @@ class ProfilePagePresenter
   end
 
   def code
-    if coupon = @user.shirt_coupon
-      coupon.code
-    elsif coupon = @user.sticker_coupon
-      coupon.code
-    end
+    coupon = @user.shirt_coupon
+    coupon&.code
+    coupon = @user.sticker_coupon
+    coupon.code
   end
 end
