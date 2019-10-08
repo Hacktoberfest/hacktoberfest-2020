0888e19dc5937a02c4539d2f4d1af3470d820dbf
Frida Casas <fridakahsas@yahoo.com>
2019-10-08 16:38:14 -0400

    Lint new coupon models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,8 @@ module ApplicationHelper
     Kramdown::Document.new(source.to_s).to_html.html_safe
   end
 
-  def active_path_class?(test_path, test_path_2)
-    return 'active' if request.path == test_path || request.path == test_path_2
+  def active_path_class?(test_path, test_path2)
+    return 'active' if request.path == test_path || request.path == test_path2
 
     ''
   end
