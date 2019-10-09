4ebb495c692b9342803f90b62b2b49e40c1f7364
Chris Ewald <chrisewald@gmail.com>
2019-10-09 14:33:31 -0400

    Improve IssueStateQueryComposer

--- app/services/issue_state_query_composer.rb
+++ app/services/issue_state_query_composer.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
-class IssueStateQueryComposer
-  def self.compose(owner_name:, repo_name:, issue_number:)
-    query = <<~GRAPHQL
-      query GetIssueState($ownerName: String!,
-                          $repoName: String!,
-                          $issueNumber: Int!) {
-        rateLimit {
-          cost
-          limit
-          remaining
-          resetAt
-          }
-          repository(owner: $ownerName,
-            name: $repoName) {
-              issue(number: $issueNumber) {
-                state
-              }
-          }
-      }'
-    GRAPHQL
+module IssueStateQueryComposer
+  module_function
+
+  ISSUE_STATE_QUERY = <<~GRAPHQL
+    query GetIssueState($ownerName: String!,
+                        $repoName: String!,
+                        $issueNumber: Int!) {
+      rateLimit {
+        cost
+        limit
+        remaining
+        resetAt
+        }
+        repository(owner: $ownerName,
+          name: $repoName) {
+            issue(number: $issueNumber) {
+              state
+            }
+        }
+    }'
+  GRAPHQL
+
+  def compose(owner_name:, repo_name:, issue_number:)
     {
-      query: query,
+      query: ISSUE_STATE_QUERY,
       variables: {
         ownerName: owner_name,
         repoName: repo_name,
