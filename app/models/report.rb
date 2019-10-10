e7100a8adf3ee9242910385cf6f83efd08dde1fd
Chris Ewald <chrisewald@gmail.com>
2019-10-10 16:14:03 -0400

    Move descriptive error message to the view for more easy control

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,16 +5,10 @@ class Report
   include ActiveModel::Model
 
   GITHUB_REPO_URL_REGEX = %r{github.com\/([\w.-]+\/[\w.-]+)}.freeze
-  INVALID_URL_MESSAGE = <<~INVALID_URL_MESSAGE.squish.freeze
-    The GitHub repository url is invalid. Please include the full url of the
-    repository including the name and owners.
-    Example: "https://github.com/user/repository"
-  INVALID_URL_MESSAGE
 
   attr_accessor :url
 
-  validates :url, format: { with: GITHUB_REPO_URL_REGEX,
-                            message: INVALID_URL_MESSAGE }
+  validates :url, format: { with: GITHUB_REPO_URL_REGEX }
 
   def save
     return false unless valid?
