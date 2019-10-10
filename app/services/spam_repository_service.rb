ef655bde43d95be51c2831cf971e356c77ff66fd
Frida Casas <fridakahsas@yahoo.com>
2019-10-10 17:47:47 -0400

    Return false if Airtable key is nil in SpamRepoService

--- app/services/spam_repository_service.rb
+++ app/services/spam_repository_service.rb
@@ -4,10 +4,14 @@ module SpamRepositoryService
   module_function
 
   def call(repo_id)
-    table = AirrecordTable.new.table('Spam Repos')
-    spam_repo_ids = table.all.map do |repo|
-      repo['Repo ID']&.to_i if repo['Verified?']
-    end.compact
-    spam_repo_ids.include?(repo_id)
+    if AirrecordTable.new.airtable_key_present?
+      table = AirrecordTable.new.table('Spam Repos')
+      spam_repo_ids = table.all.map do |repo|
+        repo['Repo ID']&.to_i if repo['Verified?']
+      end.compact
+      spam_repo_ids.include?(repo_id)
+    else
+      AirrecordTable.log_airbrake_warning
+      return false
   end
 end
