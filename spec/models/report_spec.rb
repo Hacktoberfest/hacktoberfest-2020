d23b1c168e373644b4736315d000f7ae44d02b34
Chris Ewald <chrisewald@gmail.com>
2019-10-10 20:05:23 -0400

    Better variable names

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,19 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  let(:valid_repo) { Report.new(url: 'https://github.com/rails/rails') }
-  let(:invalid_repo) { Report.new(url: 'https://github.com/boom') }
+  let(:valid_report) { Report.new(url: 'https://github.com/rails/rails') }
+  let(:invalid_report) { Report.new(url: 'https://github.com/boom') }
 
   describe '.github_repo_identifier' do
     context 'when the report is valid' do
       it 'returns the repo identifier' do
-        expect(valid_repo.github_repo_identifier).to eq('rails/rails')
+        expect(valid_report.github_repo_identifier).to eq('rails/rails')
       end
     end
 
     context 'when the report is invalid' do
       it 'returns nil' do
-        expect(invalid_repo.github_repo_identifier).to be_nil
+        expect(invalid_report.github_repo_identifier).to be_nil
       end
     end
   end
@@ -28,19 +28,19 @@ RSpec.describe Report, type: :model do
       end
 
       it 'calls the ReportAirtableUpdaterService' do
-        valid_repo.save
+        valid_report.save
         expect(ReportAirtableUpdaterService)
-          .to have_received(:call).with(valid_repo).once
+          .to have_received(:call).with(valid_report).once
       end
 
       it 'always returns true' do
-        expect(valid_repo.save).to eq(true)
+        expect(valid_report.save).to eq(true)
       end
     end
 
     context 'when the report is invalid' do
       it 'returns false' do
-        expect(invalid_repo.save).to eq(false)
+        expect(invalid_report.save).to eq(false)
       end
     end
   end
