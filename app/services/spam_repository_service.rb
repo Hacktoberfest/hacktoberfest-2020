985bac77a4e6800ffcacdd24744678caa781e614
Frida Casas <fridakahsas@yahoo.com>
2019-10-21 11:54:25 -0400

    Reset SpamRepositoryService

--- app/services/spam_repository_service.rb
+++ app/services/spam_repository_service.rb
@@ -5,7 +5,7 @@ module SpamRepositoryService
 
   def call(repo_id)
     spam_repo_ids = AirrecordTable.new.all_records('Spam Repos').map do |repo|
-      repo['Repo ID']&.to_i if repo['Verified?'] || repo['Permitted?']
+      repo['Repo ID']&.to_i if repo['Verified?']
     end.compact
     spam_repo_ids.include?(repo_id)
   end
