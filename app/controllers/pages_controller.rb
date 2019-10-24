0b807a061ec4db5b21294ca82157c513f278709c
Frida Casas <fridakahsas@yahoo.com>
2019-10-24 18:24:54 -0400

    Add global_stats instance var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
     @events = front_page_events
     @projects = ProjectService.sample(9)
     @climate_repository = ClimateProjectService.sample(3)
+    @global_stats = global_stats
   end
 
   def faqs
@@ -44,5 +45,14 @@ class PagesController < ApplicationController
       .select(&:current?)
       .sample(4)
       .sort_by(&:date)
+  end
+
+  def global_stats
+    stats_arr = [
+      { amount: '31,000', title: 'CHALLENGE COMPLETIONS' },
+      { amount: '231,000', title: 'PULL REQUESTS OPENED' },
+      { amount: '31,000', title: 'PARTICIPATING REPOSITORIES' }
+    ]
+    stats_arr = stats_arr.map { |s| Hashie::Mash.new(s) }
   end
 end
