c6528657b7a8e7d905b7446666cad0338c8c7fd7
Jacob Haff <jacobhaff2@gmail.com>
2019-10-25 11:40:47 -0400

    Destroy SpamRepositories absent in Airtable

--- app/services/fetch_spam_repositories_service.rb
+++ app/services/fetch_spam_repositories_service.rb
@@ -8,8 +8,19 @@ module FetchSpamRepositoriesService
       repo['Repo ID']&.to_i if repo['Verified?']
     end.compact
 
+    create_new_spam_repos(spam_repo_ids)
+    remove_absent_repos(spam_repo_ids)
+  end
+
+  def create_new_spam_repos(spam_repo_ids)
     spam_repo_ids.each do |spam_repo_id|
       SpamRepository.where(github_id: spam_repo_id).first_or_create
+    end
+  end
+
+  def remove_absent_repos(spam_repo_ids)
+    SpamRepository.all.map do |spam_repo|
+      spam_repo.destroy unless spam_repo_ids.include?(spam_repo.github_id)
     end
   end
 end
--- spec/services/fetch_spam_repositories_service_spec.rb
+++ spec/services/fetch_spam_repositories_service_spec.rb
@@ -5,11 +5,21 @@ require 'rails_helper'
 RSpec.describe FetchSpamRepositoriesService do
   describe '.call' do
     context 'Airtable provides banned repos' do
-
       it 'creates the correct count of Spam Repositories in our db', :vcr do
         FetchSpamRepositoriesService.call
 
         expect(SpamRepository.count).to eq(133)
+      end
+
+      it 'removes SpamRepositories absent in airtable', :vcr do
+        repo_absent_in_airtable = FactoryBot.create(:spam_repository)
+
+        FetchSpamRepositoriesService.call
+
+        expect(SpamRepository.count).to eq(133)
+        expect(
+          repo_absent_in_airtable.class.exists?(repo_absent_in_airtable.id)
+        ).to eq(false)
       end
     end
   end
