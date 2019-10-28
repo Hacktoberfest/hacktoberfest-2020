b6305b2ce012e7d773dc750891a998758069b136
Jacob Haff <jacobhaff2@gmail.com>
2019-10-28 13:43:34 -0400

    Add `ImportPRMetadataService`

---
+++ app/services/import_pr_metadata_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module ImportPRMetadataService
+  module_function
+
+  def call(user)
+    pr_data = user.pull_requests
+
+    pr_data.map do |pr|
+      binding.pry
+      PRStat.where(pr_id: pr.id).first_or_create(data: pr)
+    end
+  end
+end
