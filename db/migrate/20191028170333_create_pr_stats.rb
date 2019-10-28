3ff0b7f3c265c63a66cb95b046c934ca9c5688d1
Jacob Haff <jacobhaff2@gmail.com>
2019-10-28 13:25:14 -0400

    Add PR tat table and model

---
+++ db/migrate/20191028170333_create_pr_stats.rb
@@ -0,0 +1,10 @@
+class CreatePrStats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pr_stats do |t|
+      t.jsonb :data, null: false
+      t.string :pr_id, unique: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_143145) do
+ActiveRecord::Schema.define(version: 2019_10_28_170333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_10_28_143145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_languages_on_name", unique: true
+  end
+
+  create_table "pr_stats", force: :cascade do |t|
+    t.jsonb "data", null: false
+    t.string "pr_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pull_requests", force: :cascade do |t|
