9d07fc15e2ddad6ecb175b2dc39c9fae82e40080
Jacob Haff <jacobhaff2@gmail.com>
2019-10-30 18:05:06 -0400

    Structural and linting fixes to import_user_metadata_service_spec

--- spec/services/import_user_metadata_service_spec.rb
+++ spec/services/import_user_metadata_service_spec.rb
@@ -4,11 +4,14 @@ require 'rails_helper'
 
 RSpec.describe ImportUserMetadataService do
   describe '.call' do
-    let(:user) { FactoryBot.create(:user) }
-    before do
-      allow_any_instance_of(Octokit::Client).to receive(:user).and_return({"test": 1})
-    end
     context 'Octokit returns user data' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        allow_any_instance_of(Octokit::Client)
+          .to receive(:user).and_return("test": 1)
+      end
+
       context 'the user is absent in the user stats table' do
         it 'creates a UserStat' do
           ImportUserMetadataService.call(user)
@@ -17,7 +20,9 @@ RSpec.describe ImportUserMetadataService do
         end
       end
       context 'the user is present in the user stats table' do
-        let!(:existing_stat) { UserStat.create(user_id: user.id, data: {"test": 1})}
+        let!(:existing_stat) do
+          UserStat.create(user_id: user.id, data: { "test": 1 })
+        end
 
         it 'does not create another UserStat' do
           ImportUserMetadataService.call(user)
