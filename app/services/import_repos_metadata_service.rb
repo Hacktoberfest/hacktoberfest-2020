6b445ea7bc1d9f42ac5ab19e930adec831ce9a4d
Jacob Haff <jacobhaff2@gmail.com>
2019-10-30 18:05:40 -0400

    Remove unneeded guard clause

--- app/services/import_repos_metadata_service.rb
+++ app/services/import_repos_metadata_service.rb
@@ -7,8 +7,6 @@ module ImportReposMetadataService
     pr_service = PullRequestService.new(user, randomize_token: true)
     pr_data = pr_service.all
 
-    return unless pr_data
-
     pr_data.map do |pr|
       repo_id = pr.github_pull_request.repo_id
       ImportRepoMetadataJob.perform_async(repo_id)
