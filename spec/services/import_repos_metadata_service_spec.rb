67d9b66273a5ac1162636ccef8aef0bf8ae68084
Jacob Haff <jacobhaff2@gmail.com>
2019-10-30 15:06:41 -0400

    Add import_repos_metadata_service_spec

---
+++ spec/services/import_repos_metadata_service_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ImportReposMetadataService do
+  describe '.call' do
+    let(:user) { FactoryBot.create(:user) }
+    context 'The user has no PRs' do
+      before do
+        allow_any_instance_of(PullRequestService)
+            .to receive(:all).and_return(nil)
+      end
+      
+      it 'enqueues no ImportRepoMetadataJobs' do
+        ImportReposMetadataService.call(user)
+
+        expect(ImportRepoMetadataJob).to_not receive(:perform_async)
+      end
+    end
+  end
+end
