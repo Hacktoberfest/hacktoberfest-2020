81c992d3927fece1700bcedac8c901ce26f85236
Frida Casas <fridakahsas@yahoo.com>
2019-10-30 16:30:09 -0400

    Lint user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -31,7 +31,7 @@ FactoryBot.define do
       state { 'completed' }
 
       # setting receipt data here for validations, else the create fails
-      receipt { {'test': 'test' }.to_json }
+      receipt { { 'test': 'test' }.to_json }
 
       after :build do |user|
         allow(user).to receive(:eligible_pull_requests_count).and_return(4)
@@ -41,7 +41,7 @@ FactoryBot.define do
 
     trait :incompleted do
       state { 'incompleted' }
-      receipt { {'test': 'test' }.to_json }
+      receipt { { 'test': 'test' }.to_json }
 
       after :build do |user|
         allow(user).to receive(:eligible_pull_requests_count).and_return(3)
@@ -51,65 +51,65 @@ FactoryBot.define do
 
     trait :won_shirt do
       state { 'won_shirt' }
-      receipt { {'test': 'test' }.to_json }
+      receipt { { 'test': 'test' }.to_json }
       shirt_coupon
     end
 
     trait :won_sticker do
       state { 'won_sticker' }
-      receipt { {'test': 'test' }.to_json }
+      receipt { { 'test': 'test' }.to_json }
       sticker_coupon
     end
 
     trait :winner_with_receipt do
       state { 'won_shirt' }
-      receipt {
-        [{'github_pull_request'=>
-          {'graphql_hash'=>
-             {'id'=>'MDExOlB1bGxSZXF1ZXN0MzI1NDc3OTA3',
-              'url'=>'https://github.com/raise-dev/hacktoberfest-test/pull/18',
-              'body'=>'',
-              'title'=>'Peculiar branch',
-              'labels'=>{'edges'=>[]},
-              'createdAt'=>'2019-10-07T20:23:23Z',
-              'repository'=>{'databaseId'=>211178535}}}},
-          {'github_pull_request'=>
-            {'graphql_hash'=>
-              {'id'=>'MDExOlB1bGxSZXF1ZXN0MzI1NDc3OTY1',
-              'url'=>'https://github.com/raise-dev/hacktoberfest-test/pull/19',
-              'body'=>'',
-              'title'=>'Fun branch',
-              'labels'=>{'edges'=>[]},
-              'createdAt'=>'2019-10-07T20:23:33Z',
-              'repository'=>{'databaseId'=>211178535}}}},
-          {'github_pull_request'=>
-            {'graphql_hash'=>
-              {'id'=>'MDExOlB1bGxSZXF1ZXN0MzI1NDc4MDIy',
-              'url'=>'https://github.com/raise-dev/hacktoberfest-test/pull/20',
-              'body'=>'',
-              'title'=>'Super branch',
-              'labels'=>{'edges'=>[]},
-              'createdAt'=>'2019-10-07T20:23:42Z',
-              'repository'=>{'databaseId'=>211178535}}}},
-          {'github_pull_request'=>
-            {'graphql_hash'=>
-              {'id'=>'MDExOlB1bGxSZXF1ZXN0MzI1NDc4MDc2',
-              'url'=>'https://github.com/raise-dev/hacktoberfest-test/pull/21',
-              'body'=>'',
-              'title'=>'readme addition',
-              'labels'=>{'edges'=>[]},
-              'createdAt'=>'2019-10-07T20:23:51Z',
-              'repository'=>{'databaseId'=>211178535}}}},
-          {'github_pull_request'=>
-            {'graphql_hash'=>
-              {'id'=>'MDExOlB1bGxSZXF1ZXN0MzI1NDgxMzA4',
-              'url'=>'https://github.com/raise-dev/hacktoberfest-test/pull/22',
-              'body'=>'',
-              'title'=>'Fakerson',
-              'labels'=>{'edges'=>[{'node'=>{'name'=>'invalid'}}]},
-              'createdAt'=>'2019-10-07T20:32:11Z',
-              'repository'=>{'databaseId'=>211178535}}}}]
-          }
+      receipt do
+        [{ 'github_pull_request' =>
+          { 'graphql_hash' =>
+             { 'id' => 'MDExOlB1bGxSZXF1ZXN0MzI1NDc3OTA3',
+               'url' => 'https://github.com/raise-dev/hacktoberfest-test/pull/18',
+               'body' => '',
+               'title' => 'Peculiar branch',
+               'labels' => { 'edges' => [] },
+               'createdAt' => '2019-10-07T20:23:23Z',
+               'repository' => { 'databaseId' => 211_178_535 } } } },
+         { 'github_pull_request' =>
+           { 'graphql_hash' =>
+             { 'id' => 'MDExOlB1bGxSZXF1ZXN0MzI1NDc3OTY1',
+               'url' => 'https://github.com/raise-dev/hacktoberfest-test/pull/19',
+               'body' => '',
+               'title' => 'Fun branch',
+               'labels' => { 'edges' => [] },
+               'createdAt' => '2019-10-07T20:23:33Z',
+               'repository' => { 'databaseId' => 211_178_535 } } } },
+         { 'github_pull_request' =>
+           { 'graphql_hash' =>
+             { 'id' => 'MDExOlB1bGxSZXF1ZXN0MzI1NDc4MDIy',
+               'url' => 'https://github.com/raise-dev/hacktoberfest-test/pull/20',
+               'body' => '',
+               'title' => 'Super branch',
+               'labels' => { 'edges' => [] },
+               'createdAt' => '2019-10-07T20:23:42Z',
+               'repository' => { 'databaseId' => 211_178_535 } } } },
+         { 'github_pull_request' =>
+           { 'graphql_hash' =>
+             { 'id' => 'MDExOlB1bGxSZXF1ZXN0MzI1NDc4MDc2',
+               'url' => 'https://github.com/raise-dev/hacktoberfest-test/pull/21',
+               'body' => '',
+               'title' => 'readme addition',
+               'labels' => { 'edges' => [] },
+               'createdAt' => '2019-10-07T20:23:51Z',
+               'repository' => { 'databaseId' => 211_178_535 } } } },
+         { 'github_pull_request' =>
+           { 'graphql_hash' =>
+             { 'id' => 'MDExOlB1bGxSZXF1ZXN0MzI1NDgxMzA4',
+               'url' => 'https://github.com/raise-dev/hacktoberfest-test/pull/22',
+               'body' => '',
+               'title' => 'Fakerson',
+               'labels' => { 'edges' => [{ 'node' => { 'name' => 'invalid' } }] },
+               'createdAt' => '2019-10-07T20:32:11Z',
+               'repository' => { 'databaseId' => 211_178_535 } } } }]
+      end
       shirt_coupon
     end
 
