42e0f0e61cf7c259e2c1db3f787913cceead1e9f
lpercivalDEV <lpercival.dev@gmail.com>
2019-10-30 10:27:15 -0400

    Fix bonus score method

--- app/presenters/profile_page_presenter.rb
+++ app/presenters/profile_page_presenter.rb
@@ -51,11 +51,7 @@ class ProfilePagePresenter
   end
 
   def bonus_score
-    if @user.completed? || @user.won_shirt? || @user.won_sticker?
-      (@user.persisted_winning_pull_requests.select(&:eligible?).count || 0) - 4
-    else
-      (@user.eligible_pull_requests_count || 0) - @user.score
-    end
+    (@user.eligible_pull_requests_count || 0) - @user.score
   end
 
   def name
