414c870441414474c46b918ae8acfa1657d6a308
lpercivalDEV <lpercival.dev@gmail.com>
2019-10-31 11:45:02 -0400

    Create sample project issues to test functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,20 @@
 User.create!
 User.create!
 User.create!
+
+10.times do
+  FactoryBot.create(:issue)
+end
+
+Language.create(name: "javascript")
+
+Language.create(name: "ruby")
+
+Language.create(name: "python")
+
+Language.create(name: "go")
+
+Repository.all.each do |r|
+  r.language_id = Language.first.id
+  r.save
+end
