ea68f52320d908131d1b4b0c04ffb234a4e66282
Jacob Haff <jacobhaff2@gmail.com>
2019-11-01 14:31:11 -0400

    Add test case for deleted user to import_repos_metadata_service

--- spec/services/import_repos_metadata_service_spec.rb
+++ spec/services/import_repos_metadata_service_spec.rb
@@ -17,5 +17,22 @@ RSpec.describe ImportReposMetadataService do
         expect(ImportRepoMetadataJob).to_not receive(:perform_async)
       end
     end
+
+    context 'The user has been deleted from Github' do
+      before do
+        allow_any_instance_of(GithubPullRequestService)
+          .to receive(:pull_requests)
+          .and_raise(GithubPullRequestService::UserDeletedError.new)
+      end
+
+      it 'updates the correct UserStat' do
+        user_stat = UserStat.create(user_id: user.id, data: user)
+
+        ImportReposMetadataService.call(user)
+
+        user_stat.reload
+        expect(user_stat.deleted).to eq(true)
+      end
+    end
   end
 end
