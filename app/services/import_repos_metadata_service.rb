d708d9b642780decf24e05f2689861e868d44462
Jacob Haff <jacobhaff2@gmail.com>
2019-11-01 14:05:07 -0400

    Rescue UserDeletedError in ImportReposMetadataService

--- app/services/import_repos_metadata_service.rb
+++ app/services/import_repos_metadata_service.rb
@@ -5,7 +5,15 @@ module ImportReposMetadataService
 
   def call(user)
     pr_service = PullRequestService.new(user, randomize_token: true)
-    pr_data = pr_service.all
+
+    begin
+      pr_data = pr_service.all
+    rescue GithubPullRequestService::UserDeletedError
+      user_stat = UserStat.where(user_id: user.id).first_or_create(data: user)
+
+      user_stat.update(deleted: true)
+      return
+    end
 
     pr_data.map do |pr|
       repo_id = pr.github_pull_request.repo_id
