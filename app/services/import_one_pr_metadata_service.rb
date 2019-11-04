bd118a75b57be56da811e660896756d953e1e897
Jacob Haff <jacobhaff2@gmail.com>
2019-11-04 12:24:15 -0500

    Refactor and break up call method of ImportOnePrMetadataService

--- app/services/import_one_pr_metadata_service.rb
+++ app/services/import_one_pr_metadata_service.rb
@@ -4,17 +4,27 @@ module ImportOnePrMetadataService
   module_function
 
   def call(url)
-    access_token = GithubTokenService.random
-    api_client = Octokit::Client.new(access_token: access_token)
+    pull_request_args = pull_request_args_from_url(url)
 
-    repo = url.split('/')[3] + '/' + url.split('/')[4]
-    number = url.split('/').last
-
-    pr_data = api_client.pull_request(repo, number).to_hash
+    pr_data = client.pull_request(
+      pull_request_args[0], pull_request_args[1]
+    ).to_hash
 
     # using node_id here because that's what we
     # used previously as the pr_id in a PRStat. This way the data is consistent
     pr = PRStat.where(pr_id: pr_data[:node_id]).first_or_create(data: pr_data)
     pr.update(data: pr_data)
+  end
+
+  def client
+    access_token = GithubTokenService.random
+    Octokit::Client.new(access_token: access_token)
+  end
+
+  def pull_request_args_from_url(url)
+    repo = url.split('/')[3] + '/' + url.split('/')[4]
+    number = url.split('/').last
+
+    [repo, number]
   end
 end
