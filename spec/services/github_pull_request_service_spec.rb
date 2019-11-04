e59f79aa3a3b555601762c46d58e7e6cb65e4377
Jacob Haff <jacobhaff2@gmail.com>
2019-11-04 11:52:20 -0500

    Linting

--- spec/services/github_pull_request_service_spec.rb
+++ spec/services/github_pull_request_service_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe GithubPullRequestService do
   describe '#pull_requests' do
     context 'user was found' do
-      let(:user) { FactoryBot.create(:user, uid: 7976757)}
+      let(:user) { FactoryBot.create(:user, uid: 7_976_757) }
       let(:github_pull_request_service) { GithubPullRequestService.new(user) }
 
       it 'returns an array of GithubPullRequests', :vcr do
@@ -15,10 +15,10 @@ RSpec.describe GithubPullRequestService do
     end
 
     context 'user was not found' do
-      let(:user) { FactoryBot.create(:user, uid: 56205871)}
+      let(:user) { FactoryBot.create(:user, uid: 56_205_871) }
       let(:github_pull_request_service) { GithubPullRequestService.new(user) }
 
-      it 'returns raises a UserDeletedError', :vcr do
+      it 'raises a UserDeletedError', :vcr do
         expect { github_pull_request_service.pull_requests }
           .to raise_error(GithubPullRequestService::UserDeletedError)
       end
