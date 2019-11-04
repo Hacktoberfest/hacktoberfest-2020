81c9561300734153054d7c4149e8ac06cca7d13b
Jacob Haff <jacobhaff2@gmail.com>
2019-11-04 14:03:38 -0500

    Destroy UserStats for users deleted from Github in ImportReposMetadataService

--- spec/services/import_repos_metadata_service_spec.rb
+++ spec/services/import_repos_metadata_service_spec.rb
@@ -25,13 +25,12 @@ RSpec.describe ImportReposMetadataService do
           .and_raise(GithubPullRequestService::UserDeletedError.new)
       end
 
-      it 'updates the correct UserStat' do
-        user_stat = UserStat.create(user_id: user.id, data: user)
+      it 'destroys the UserStat' do
+        UserStat.create(user_id: user.id, data: user)
 
         ImportReposMetadataService.call(user)
 
-        user_stat.reload
-        expect(user_stat.deleted).to eq(true)
+        expect(UserStat.count).to eq(0)
       end
     end
   end
