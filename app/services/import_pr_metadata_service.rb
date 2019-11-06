9b6dcf38aa7f547f72fdb102c415ec134a576598
Jacob Haff <jacobhaff2@gmail.com>
2019-11-06 16:54:58 -0500

    Use GithubErrorHandler in ImportPrMetadataService

--- app/services/import_pr_metadata_service.rb
+++ app/services/import_pr_metadata_service.rb
@@ -6,17 +6,12 @@ module ImportPrMetadataService
   def call(user)
     pr_service = PullRequestService.new(user, randomize_token: true)
 
-    begin
+    GithubErrorHandler.with_error_handling do
       pr_data = pr_service.all
-    rescue GithubPullRequestService::UserNotFoundOnGithubError
-      user_stat = UserStat.where(user_id: user.id).first
 
-      user_stat.destroy
-      return
-    end
-
-    pr_data.map do |pr|
-      ImportOnePrMetadataJob.perform_async(pr.url)
+      pr_data.map do |pr|
+        ImportOnePrMetadataJob.perform_async(pr.url)
+      end
     end
   end
 end
