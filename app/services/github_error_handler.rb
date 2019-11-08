119e5e6cef547385e8d0630ba7b0669756c593d6
Jacob Haff <jacobhaff2@gmail.com>
2019-11-08 11:54:50 -0500

    Add tests for GithubErrorHandler

--- app/services/github_error_handler.rb
+++ app/services/github_error_handler.rb
@@ -29,10 +29,10 @@ module GithubErrorHandler
   end
 
   def process_user_missing_error(_error, user)
+    user.deactivate
+
     return unless (user_stat = UserStat.where(user_id: user.id).first)
 
     user_stat.destroy
-
-    user.deactivate
   end
 end
