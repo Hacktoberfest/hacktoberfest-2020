48662dc62e817a5872d40c95800c0361c2eb0f36
Jacob Haff <jacobhaff2@gmail.com>
2019-11-08 11:53:57 -0500

    Fix ImportReposMetadataSerivce to call with_error_handling correctly

--- app/services/import_repos_metadata_service.rb
+++ app/services/import_repos_metadata_service.rb
@@ -6,7 +6,7 @@ module ImportReposMetadataService
   def call(user)
     pr_service = PullRequestService.new(user, randomize_token: true)
 
-    GithubErrorHandler.with_error_handling do
+    GithubErrorHandler.with_error_handling(user) do
       pr_data = pr_service.all
 
       pr_data.map do |pr|
