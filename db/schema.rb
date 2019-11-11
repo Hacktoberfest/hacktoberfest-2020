dacbaa91856ce0f9d1cf70a10013c949be78842a
Jacob Haff <jacobhaff2@gmail.com>
2019-11-11 15:01:12 -0500

    Add state_before_inactive to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_151321) do
+ActiveRecord::Schema.define(version: 2019_11_11_195936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_151321) do
     t.datetime "waiting_since"
     t.jsonb "receipt"
     t.string "last_error"
+    t.string "state_before_inactive"
   end
 
 end
