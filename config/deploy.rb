1db2489dd32ac7f3e5e92085771c5ff407bbfb3b
John Britton <public@johndbritton.com>
2019-11-11 22:22:56 -0500

    Ask what branch to deploy, default to active branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ lock '~> 3.11.1'
 set :application, 'hacktoberfest'
 set :repo_url, 'git@github.com:raise-dev/hacktoberfest.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Ask which branch to deploy
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/hacktoberfest'
