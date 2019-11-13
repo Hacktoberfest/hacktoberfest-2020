b26ef905d4fe5879e384750232d30904d952574b
Frida Casas <fridacasas.fc@gmail.com>
2019-11-13 12:03:44 -0500

    Add gifted_sticker method to  UserStateTransitionSegmentService

--- app/services/user_state_transition_segment_service.rb
+++ app/services/user_state_transition_segment_service.rb
@@ -15,6 +15,7 @@ module UserStateTransitionSegmentService
     when :incomplete then incomplete(user)
     when :ineligible then ineligible(user)
     when :won then won(user, transition)
+    when :gifted_sticker then gift_sticker(user)
     end
   end
 
@@ -59,6 +60,13 @@ module UserStateTransitionSegmentService
         state: 'won_sticker'
       )
     end
+  end
+
+  def gift_sticker(user)
+    segment(user).track('user_gifted_sticker')
+    segment(user).identify(
+      state: 'gifted_sticker'
+    )
   end
 
   def segment(user)
