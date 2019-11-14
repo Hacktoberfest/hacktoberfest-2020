c69bb140223f1d4e83e3ddefbbe39ef059584941
Frida Casas <fridacasas.fc@gmail.com>
2019-11-14 11:46:05 -0500

    Fix sorting logic

--- app/services/gift_stickers_service.rb
+++ app/services/gift_stickers_service.rb
@@ -8,20 +8,21 @@ module GiftStickersService
 
     User.where(state: 'incompleted').find_in_batches do |group|
       group.each do |u|
-        date = u.receipt.max_by do |receipt|
+        date = u.receipt.min_by do |receipt|
           Time.zone.parse(
             receipt['github_pull_request']['graphql_hash']['createdAt']
           )
         end['github_pull_request']['graphql_hash']['createdAt']
-
         score = u.receipt.count
         user_dates << { id: u.id, score: score, date: date }
       end
     end
 
     sorted = user_dates.sort do |a, b|
-      [a.score, Time.zone.parse(a.date)] <=> [b.score, Time.zone.parse(b.date)]
+      [b[:score], Time.zone.parse(a[:date])] <=> [a[:score], Time.zone.parse(b[:date])]
     end
+
+    binding.pry
 
     sorted.each do |user_date|
       u = User.find(user_date[:id])
