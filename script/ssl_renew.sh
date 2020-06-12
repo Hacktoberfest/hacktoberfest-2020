43aa3668356fc3fccd24bef0dba88e508f31c140
Kat Juell <kathleenjuell@gmail.com>
2020-06-12 15:52:56 -0400

    Remove dry run option for renew

--- script/ssl_renew.sh
+++ script/ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/sammy/hacktoberfest/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
