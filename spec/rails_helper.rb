172523062b4f40c2d8c96eb684498170a040f570
Kat Juell <kathleenjuell@gmail.com>
2020-08-05 17:25:44 -0400

    Add config for testing Sidekiq to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails env is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'sidekiq/testing'
+
+Sidekiq::Testing.fake!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
