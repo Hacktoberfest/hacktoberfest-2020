38ea07cd80e1dc1bf292d17fec3d3792dc60aa69
Kat Juell <kathleenjuell@gmail.com>
2020-08-05 17:45:21 -0400

    Add empty values to test yaml

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,6 @@ abort('The Rails env is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'sidekiq/testing'
 
-Sidekiq::Testing.fake!
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
