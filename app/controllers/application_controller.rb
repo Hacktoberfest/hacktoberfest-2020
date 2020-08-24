5e74fa1454c17f06d31cfb676b8ce0555b6f83a0
Kat Juell <kathleenjuell@gmail.com>
2020-08-24 15:31:29 -0400

    Redirect to registration form in require_user_registered method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
     if Hacktoberfest.ended?
       render 'pages/hacktoberfest_ended'
     else
-      redirect_to start_path
+      redirect_to register_path
     end
   end
 
