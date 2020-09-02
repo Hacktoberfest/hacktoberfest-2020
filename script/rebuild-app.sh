73e8c9b8360f79c6d368022f2b97c92e3ad33ba7
Kat Juell <kathleenjuell@gmail.com>
2020-09-02 17:42:21 -0400

    Newlines for all

--- script/rebuild-app.sh
+++ script/rebuild-app.sh
@@ -7,4 +7,5 @@ RESTART_FILE="docker-compose.restart.yml"
 
 $COMPOSE stop app sidekiq && $COMPOSE rm -f app sidekiq
 $DOCKER volume rm hacktoberfest_gem_cache
-$COMPOSE -f $FILE -f $RESTART_FILE up -d --build --force-recreate --no-deps app sidekiq 
+$COMPOSE -f $FILE -f $RESTART_FILE up -d --build --force-recreate --no-deps app sidekiq
+ 
