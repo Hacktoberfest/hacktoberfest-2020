46b7b929c38641372dd0c7e8db8673dd9f79e924
Kat Juell <kathleenjuell@gmail.com>
2020-09-08 13:25:49 -0400

    Add config setting to use RAILS_MASTER_KEY

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   config.cache_store = :dalli_store, *ENV['DALLI_SERVER']
 
-  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  config.require_master_key = true
 
   config.eager_load = true
 
