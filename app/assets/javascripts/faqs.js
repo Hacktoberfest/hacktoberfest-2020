73afa899bae2032c985b04ec61fa1d7408812080
Kat Juell <kathleenjuell@gmail.com>
2020-09-23 09:44:33 -0400

    Remove unneeded preventdefault

--- app/assets/javascripts/faqs.js
+++ app/assets/javascripts/faqs.js
@@ -1,6 +1,5 @@
 window.addEventListener('click', (e) => {
 	var $target = $(e.target);
-	e.preventDefault();
 	if ($target.hasClass('toggle')) {
 		$target.toggleClass('active');
 		$target.next().toggleClass('answer-show');
