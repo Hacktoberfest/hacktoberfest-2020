22f5af0ad9545c79644e13fd65fd22d2f4ed0094
Kat Juell <kathleenjuell@gmail.com>
2020-09-29 08:21:49 -0400

    Fix spec

--- spec/services/hacktoberfest_project_query_composer_spec.rb
+++ spec/services/hacktoberfest_project_query_composer_spec.rb
@@ -3,14 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe HacktoberfestProjectQueryComposer do
+  # rubocop:disable Metrics/LineLength
   describe '.compose' do
     context 'When not given a cursor' do
       it 'returns a project graphql json query for the first set of results' do
         results_per_page = 1_000_000
+        date = Time.zone.now.strftime('%Y-%m-%d')
         graphql_json_query_without_pagination = {
           query: HacktoberfestProjectQueryComposer::PROJECT_IMPORT_QUERY,
           variables: {
-            queryString: 'state:open label:hacktoberfest no:assignee',
+            queryString: 'state:open label:hacktoberfest no:assignee created:' + date,
             first: results_per_page
           }
         }
@@ -28,10 +30,11 @@ RSpec.describe HacktoberfestProjectQueryComposer do
       it 'returns a project graphql json query paginated to the cursor' do
         results_per_page = 1_000_000
         cursor = 'a_dog_eared_page'
+        date = Time.zone.now.strftime('%Y-%m-%d')
         graphql_json_query_with_pagination = {
           query: HacktoberfestProjectQueryComposer::PROJECT_IMPORT_QUERY,
           variables: {
-            queryString: 'state:open label:hacktoberfest no:assignee',
+            queryString: 'state:open label:hacktoberfest no:assignee created:' + date,
             first: results_per_page,
             after: cursor
           }
@@ -50,10 +53,11 @@ RSpec.describe HacktoberfestProjectQueryComposer do
       it 'returns a project graphql json query with the query string' do
         results_per_page = 1_000_000
         q_str = 'language:Ruby'
+        date = Time.zone.now.strftime('%Y-%m-%d')
         graphql_json_query_with_pagination = {
           query: HacktoberfestProjectQueryComposer::PROJECT_IMPORT_QUERY,
           variables: {
-            queryString: "state:open label:hacktoberfest no:assignee #{q_str}",
+            queryString: "state:open label:hacktoberfest no:assignee #{q_str} created:" + date,
             first: results_per_page
           }
         }
@@ -68,4 +72,5 @@ RSpec.describe HacktoberfestProjectQueryComposer do
       end
     end
   end
+  # rubocop:enable Metrics/LineLength
 end
