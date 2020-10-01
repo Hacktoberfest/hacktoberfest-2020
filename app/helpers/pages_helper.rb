6f22fd0358835c2a0347137593fbceb61a783bf1
Kat Juell <kathleenjuell@gmail.com>
2020-09-30 21:04:05 -0400

    Make sure sample featured events also work

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,7 +8,7 @@ module PagesHelper
       # Ignore invalid events
     end.compact
   rescue JSON::ParserError
-    AirtablePlaceholderService.call('Event List').map do |e|
+    AirtablePlaceholderService.call('Meetups').map do |e|
       MlhEventPresenter.new(e)
     end.compact
   end
