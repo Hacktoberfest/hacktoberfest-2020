2a04f387e04fdddaae88eb45a8cca407dfded55c
Kat Juell <kathleenjuell@gmail.com>
2020-10-02 15:48:27 -0400

    Further styles

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,7 +1,8 @@
 window.addEventListener("click", (e) => {
     $target = $(e.target),
     $form_first = $('#form-first'),
-    $form_second = $('#form-second');
+    $form_second = $('#form-second'),
+    $video = Wistia.api('u8up7h2m32');
 
     if ($target.attr('data-js') === "continue" ) {
         $form_first.toggleClass('hidden');
@@ -10,4 +11,9 @@ window.addEventListener("click", (e) => {
         $form_first.toggleClass('hidden');
         $form_second.toggleClass('hidden');
     }
+
+    $video.bind("end", function(t) {
+        //logic here
+    });
+
 });
