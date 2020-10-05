05dc3c855c0e7ab987cf7cef4de67ac44786d3a2
Kat Juell <kathleenjuell@gmail.com>
2020-10-05 17:57:10 -0400

    Fix Wistia js errors

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -2,7 +2,6 @@ window.addEventListener("click", (e) => {
     $target = $(e.target),
     $form_first = $('#form-first'),
     $form_second = $('#form-second'),
-    $video = Wistia.api('u8up7h2m32'),
     $quality_checkers = $('.check-extra')
 
     if ($target.attr('data-js') === "continue" ) {
@@ -12,11 +11,4 @@ window.addEventListener("click", (e) => {
         $form_first.toggleClass('hidden');
         $form_second.toggleClass('hidden');
     }
-
-    $video.bind("end", function() {
-        $quality_checkers.each(function() {
-            $(this).removeAttr('disabled');     
-        });
-    });
-
 });
