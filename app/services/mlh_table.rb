b16382bad21e74d2916349b8f295d8cc5a0eea85
Kat Juell <kathleenjuell@gmail.com>
2020-10-05 10:53:54 -0400

    Ensure rescue/placeholder works on API call itself without intervention of helper methods

--- app/services/mlh_table.rb
+++ app/services/mlh_table.rb
@@ -28,11 +28,9 @@ class MlhTable
         )
       end
     end
-    if response.success?
-      response.body
-    else
-      AirtablePlaceholderService.call('Meetups')
-    end
+    response.body if response.success?
+  rescue StandardError
+    { 'data' => AirtablePlaceholderService.call('Meetups') }
   end
 
   def records
