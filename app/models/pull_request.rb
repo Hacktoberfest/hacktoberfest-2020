706dbc035eb55f4af793d47ec72f4e4b27740ecb
MattIPv4 <me@mattcowley.co.uk>
2020-10-07 11:04:08 +0100

    Rubocop

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
 class PullRequest < ApplicationRecord
   attr_reader :github_pull_request
 
@@ -136,3 +137,4 @@ class PullRequest < ApplicationRecord
     pr
   end
 end
+# rubocop:enable Metrics/ClassLength
