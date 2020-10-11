e3c397826a5a2f885850177983666481cb04e360
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-10-11 14:24:30 -0400

    Make locale sticky as the user navigates the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,8 +50,12 @@ class ApplicationController < ActionController::Base
     redirect_to profile_path
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   private
- 
+
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
